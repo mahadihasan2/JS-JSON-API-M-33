e9d5bc8689bc40b3a0b3f735153e2737293fec08
mahadihasan2 <mahadihasan125643@gmail.com>
2022-02-22 14:25:05 +0600

    Dynamic data call for API

--- JS/api.js
+++ JS/api.js
@@ -1,23 +1,34 @@
-function loadData() {
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-        .then(response => response.json())
-        .then(json => console.log(json))
-}
+ function loadData() {
+     fetch('https://jsonplaceholder.typicode.com/todos/1')
+         .then(response => response.json())
+         .then(json => console.log(json))
+ }
 
-function loadUser() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-        .then(res => res.json())
-        .then(data => console.log(data))
-}
+ function loadUser() {
+     fetch('https://jsonplaceholder.typicode.com/users')
+         .then(res => res.json())
+         .then(data => displyUser(data))
+ }
 
-function loadPost() {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(res => res.json())
-        .then(data => console.log(data))
-}
+ function loadPost() {
+     fetch('https://jsonplaceholder.typicode.com/posts')
+         .then(res => res.json())
+         .then(data => console.log(data))
+ }
 
-function loadAlbums() {
-    fetch('https://jsonplaceholder.typicode.com/albums')
-        .then(res => res.json())
-        .then(data => console.log(data))
-}
+ function loadAlbums() {
+     fetch('https://jsonplaceholder.typicode.com/albums')
+         .then(res => res.json())
+         .then(data => console.log(data))
+ }
+
+ function displyUser(data) {
+     const ul = document.getElementById('users')
+     for (const user of data) {
+         const li = document.createElement('li')
+         li.innerText = `Name:${user.name},E-main:${user.email}`
+         ul.appendChild(li)
+
+
+     }
+ }
